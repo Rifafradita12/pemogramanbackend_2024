26b85e4cda923bdeb3238a5d5a866d6195b3287c
Rifafradita12 <rifafraditasafara@gmail.com>
2024-11-14 16:31:59 +0700

    upload praktikum 7

--- praktikum-laravel/routes/api.php
+++ praktikum-laravel/routes/api.php
@@ -25,6 +25,8 @@ Route::get('/students', [StudentController::class, 'index']);
 Route::post('/students', [StudentController::class, 'store']);
 Route::put('/students/{id}', [StudentController::class, 'update']);
 Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+Route::patch('/students/{id}', [StudentController::class, 'partialUpdate']);
+
 
 
 
